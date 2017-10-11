b51cf8876c8eeffcbb064bdc28f524720fbdf667
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-11 12:28:33 +0200

    invoquer appservice/modif logback

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -5,6 +5,9 @@ package dev;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import dev.service.AppService;
+
+
 
 /**
  * @author ETY8
@@ -13,9 +16,12 @@ import org.slf4j.LoggerFactory;
 public class App {
 
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	
 	public static void main(String[] args) {
+		
 	LOG.info("Bonjour Slf4J !");
 	LOG.info("Implémentation Logback");
+	new AppService().executer("valeur 1");
 	
 	}
 }
--- src/main/java/dev/service/AppService.java
+++ src/main/java/dev/service/AppService.java
@@ -3,10 +3,20 @@
  */
 package dev.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author ETY8
  *
  */
 public class AppService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(AppService.class);
+	
+	public void executer(String param) {
+	LOG.debug("Traitement 1 : param = {}", param);
+	
+	}
 
 }
