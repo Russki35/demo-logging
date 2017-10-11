187a916430c008cf8b664ad1b442b3d40fb1657e
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-11 12:05:08 +0200

    demo-logging first commit

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dev;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author ETY8
+ *
+ */
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+	public static void main(String[] args) {
+	LOG.info("Bonjour Slf4J !");
+	LOG.info("Implémentation Logback");
+	}
+}
